b20a355e7bfd566ccee144a5f88edfe462b77b92
Mattrix01 <emjay.knight@gmail.com>
2023-04-25 09:54:46 +0100

    gmae and player class, assets added

--- main.js
+++ main.js
@@ -5,4 +5,17 @@ window.addEventListener("load", function () {
   canvas.width = 500;
   // keep 500 as background images optimised for this height.
   canvas.height = 500;
+
+  // Main brain for project in here
+  class Game {
+    constructor(width, height) {
+      // converted argument width and height into class properties
+      this.width = width;
+      this.height = height;
+    }
+    // update method will run for every animation frame and trigger all calculations that need to happen
+    update() {}
+    //draw method will draw all images and score etc.
+    draw() {}
+  }
 });
---
+++ player.js
@@ -0,0 +1,6 @@
+// code to draw and update the player character.
+class Player {
+  constructor(game) {
+    this.game = game;
+  }
+}
